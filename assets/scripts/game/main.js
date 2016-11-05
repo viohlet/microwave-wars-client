3a1341396cb5d9c0b51edd27a2b511defdae9a90
viohlet <violetavenegas@hotmail.com>
2016-11-05 13:28:56 -0400

    Main character can move.
    Main character 'katie' can move around. Error: Tables and door are in
    the left now after adding static property. Next steps:
    -Find a better png for tables.
    -Achieve that katie can move around the tables and the environment.

--- assets/scripts/game/main.js
+++ assets/scripts/game/main.js
@@ -6,6 +6,8 @@
 // const api = require('./api');
 // const ui = require('./ui');
 
+// let Phaser;
+
 const game = new Phaser.Game(800, 600, Phaser.AUTO, '',{
   preload: preload,
   create: create,
@@ -19,100 +21,49 @@ function preload(){
   game.scale.pageAlignVertically = true;
   game.stage.backgroundColor = '#000';
   game.load.image('ground', './assets/scripts/game/images/ground.jpg');
-  // game.load.spritesheet('rest_st_01', './assets/scripts/game/images/spritesheet-mw.png', 32, 48);
   game.load.atlasJSONHash(
     'sprites',
-    './assets/scripts/game/images/spritesheet-mw.png',
-    './assets/scripts/game/images/spritesheet-mw.json'
+    './assets/scripts/game/images/spritesheet.png',
+    './assets/scripts/game/images/spritesheet.json'
   );
-  game.load.image('door', './assets/scripts/game/images/door.png');
-
 }
 
 let cursors;
-
+let katie;
+// let ground;
+// let body;
 
 function create() {
 
-  // // Enable P2
-  // game.physics.startSystem(Phaser.Physics.P2JS);
-  //
-  // game.physics.p2.setImpactEvents(true);
-  //
-  // game.physics.p2.restitution = 0.8;
-  //
-  // let playerCollisionGroup = game.physics.p2.createCollisionGroup();
-  // let studentsCollisionGroup = game.physics.p2.createCollisionGroup();
-  //
-  // game.physics.p2.updateBoundsCollisionGroup();
-
   let ground = game.add.image(0, 0, 'ground');
+  // ground.fixedToCamera = true;
 
-   let katie = game.add.sprite (
-     650,
-     210,
-     'sprites',
-     'rest_st_03.png'
-   );
+  //	Enable p2 physics
+	game.physics.startSystem(Phaser.Physics.P2JS);
 
-   let table1 = game.add.sprite (
-     430,
-     155,
-     'sprites',
-     'projector.png'
-   );
+  game.physics.p2.defaultRestitution = 0.8;
 
-   let table2 = game.add.sprite (
-     45,
-     155,
-     'sprites',
-     'projector.png'
-   );
+  // main character
+  katie = game.add.sprite (650, 210, 'sprites','katie.png');
+  game.physics.p2.enable(katie);
 
-   let table3 = game.add.sprite (
-     390,
-     280,
-     'sprites',
-     'projector.png'
-   );
-
-   let table4 = game.add.sprite (
-     0,
-     280,
-     'sprites',
-     'projector.png'
-   );
-
-   let table5 = game.add.sprite (
-     200,
-     390,
-     'sprites',
-     'projector.png'
-   );
-
-   let table6 = game.add.sprite (
-     430,
-     495,
-     'sprites',
-     'projector.png'
-    );
-
-    let table7 = game.add.sprite (
-      400,
-      60,
-      'sprites',
-      'projector.png'
-     );
-
-    let door = game.add.image (
-      2,
-      500,
-      'door'
-    );
+  katie.body.setZeroDamping();
+	katie.body.fixedRotation = true;
 
 
+  //tables
+  let table1 = game.add.sprite (430, 155, 'sprites', 'projector.png');
+  let table2 = game.add.sprite (45, 155, 'sprites', 'projector.png');
+  let table3 = game.add.sprite (390, 280, 'sprites', 'projector.png');
+  let table4 = game.add.sprite (0, 280, 'sprites', 'projector.png');
+  let table5 = game.add.sprite (200, 390, 'sprites', 'projector.png');
+  let table6 = game.add.sprite (430, 495, 'sprites', 'projector.png');
+  let table7 = game.add.sprite (400, 60, 'sprites', 'projector.png');
 
-  katie.scale.setTo(0.08,0.08);
+  //door
+  let door = game.add.sprite (2, 500, 'sprites', 'door.png');
+
+  katie.scale.setTo(0.4,0.4);
   table1.scale.setTo(0.6,0.3);
   table2.scale.setTo(0.6,0.3);
   table3.scale.setTo(0.6,0.3);
@@ -121,8 +72,11 @@ function create() {
   table6.scale.setTo(0.6,0.3);
   table7.scale.setTo(0.4,0.3);
   door.scale.setTo(0.5,0.5);
+  // students.scale.setTo(0.4,0.4);
 
-//make static
+  game.physics.p2.enable([ table1, table2, table3, table4, table5, table6, table7, door ]);
+
+  //make static
   table1.body.static = true;
   table2.body.static = true;
   table3.body.static = true;
@@ -132,9 +86,39 @@ function create() {
   table7.body.static = true;
   door.body.static = true;
 
-
+  cursors = game.input.keyboard.createCursorKeys();
 
 }
 
+
 function update() {
+
+  // let cursors = game.input.keyboard.createCursorKeys();
+
+  katie.body.setZeroVelocity();
+
+  if (cursors.left.isDown)
+    {
+      katie.body.moveLeft(200);
+    }
+  else if (cursors.right.isDown)
+    {
+      katie.body.moveRight(200);
+    }
+  if (cursors.up.isDown)
+    {
+      katie.body.moveUp(200);
+    }
+  else if (cursors.down.isDown)
+    {
+      katie.body.moveDown(200);
+    }
+  // if (!game.camera.atLimit.x)
+  // {
+  //   ground.tilePosition.x += (katie.body.velocity.x * 16) * game.time.physicsElapsed;
+  // }
+  // if (!game.camera.atLimit.y)
+  // {
+  //   ground.tilePosition.y += (katie.body.velocity.y * 16) * game.time.physicsElapsed;
+  // }
 }
