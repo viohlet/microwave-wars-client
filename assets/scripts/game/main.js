59791810c0156cffad08410001902fc0e75079ef
viohlet <violetavenegas@hotmail.com>
2016-11-06 18:48:38 -0500

    Collision and action with various students success
    Collision and action with various students success. All GA-14 characters
    present in the environment. All collide with main character 'katie' and
    when she 'pushes' them twice, they disappear (action success).
    Next steps:
    - Make tables/door not only static but also responsive to collisions.
    - Add scores.
    - Add timer.

--- assets/scripts/game/main.js
+++ assets/scripts/game/main.js
@@ -2,7 +2,6 @@
 
 // const getFormFields = require(`../../../lib/get-form-fields`);
 
-
 // const api = require('./api');
 // const ui = require('./ui');
 
@@ -11,7 +10,8 @@
 const game = new Phaser.Game(800, 600, Phaser.AUTO, '',{
   preload: preload,
   create: create,
-  update: update
+  update: update,
+  render: render,
 });
 
 
@@ -23,15 +23,22 @@ function preload(){
   game.load.image('ground', './assets/scripts/game/images/ground.jpg');
   game.load.atlasJSONHash(
     'sprites',
-    './assets/scripts/game/images/spritesheet.png',
-    './assets/scripts/game/images/spritesheet.json'
+    './assets/scripts/game/images/spritesheet-mini.png',
+    './assets/scripts/game/images/spritesheet-mini.json'
+  );
+  game.load.atlasJSONHash(
+    'enemies',
+    './assets/scripts/game/images/students.png',
+    './assets/scripts/game/images/students.json'
   );
 }
 
 let cursors;
 let katie;
-let ground;
-// let body;
+let wills;
+// let table1;
+let chair;
+
 
 function create() {
 
@@ -41,19 +48,97 @@ function create() {
   //	Enable p2 physics
 	game.physics.startSystem(Phaser.Physics.P2JS);
 
+// ----------------------------------------------------------
+// Turn on impact events for the world, without this we get no collision callbacks
+  game.physics.p2.setImpactEvents(true);
+// ----------------------------------------------------------
+
   game.physics.p2.defaultRestitution = 0.8;
 
+
+// ----------------------------------------------------------
+
+  // Collission Groups
+  let playerCollisionGroup = game.physics.p2.createCollisionGroup();
+  let studentCollisionGroup = game.physics.p2.createCollisionGroup();
+  let furnitureCollisionGroup = game.physics.p2.createCollisionGroup();   //// Trying tables collision
+  // Objects with their own collision groups still collide with the world bounds:
+  game.physics.p2.updateBoundsCollisionGroup();
+// -----------------------------------------------------------
+
+  // random location
+  let x = game.world.randomX;
+  let y = game.world.randomY;
+
   // main character
-  katie = game.add.sprite (650, 390, 'sprites','katie.png');
+  katie = game.add.sprite (700, 230, 'sprites','katie.png');
   game.physics.p2.enable(katie);
   katie.body.setCircle(30);
 
-  katie.body.setZeroDamping();
+  // katie.body.setZeroDamping();
 	katie.body.fixedRotation = true;
+  // -----------------------------------------------------------
+  katie.smoothed = false;
+
+  katie.body.setCollisionGroup(playerCollisionGroup);
+  katie.body.collides(studentCollisionGroup, hitStudent, hitChair);
+
+  game.camera.follow(katie);
+  // -----------------------------------------------------------
+
+  // students.body.velocity.setTo(-10, 0);
+
+  // -----------------------------------------------------------
+  let students = game.add.group();
+  students.enableBody = true;
+  students.physicsBodyType = Phaser.Physics.P2JS;
+
+  for (let i = 0; i < 23; i++)
+  {
+    let student = students.create(190 + 69 * i, -90, 'enemies', i);
+    // student.body.setRectangle(1,2,10,10);
+    // student.body.setCircle(30);
+
+    student.body.fixedRotation = true;
+
+    student.body.setCollisionGroup(studentCollisionGroup);
+
+    student.body.collides([ studentCollisionGroup, playerCollisionGroup ]);
+  }
+  // -----------------------------------------------------------
 
 
-  //tables
+  // ----------------------------------------------------------- Trying tables collision
+
+  // let furnitures = game.add.group();
+  // furnitures.enablebody = true;
+  // furnitures.physicsBodyType = Phaser.Physics.P2JS;
+  //
+  // for (let i = 0; i < 1; i++)
+  // {
+  //   let furniture = furnitures.create(50, 50, 'sprites', 'chair.png');
+  // //   furniture.body.fixedRotation = true;
+  // //
+  //   furniture.body.setCollisionGroup(furnitureCollisionGroup);
+  //
+  //   furniture.body.collides([ studentCollisionGroup, playerCollisionGroup ]);
+  // }
+
+  // chair = game.add.sprite (50, 50, 'sprites','chair.png');
+  // game.physics.p2.enable(chair);
+  // // chair.body.setCircle(30);
+  //
+  // // chair.body.setZeroDamping();
+  // chair.body.fixedRotation = true;
+  // // -----------------------------------------------------------
+  // // chair.body.static = true;
+  // chair.body.setCollisionGroup(furnitureCollisionGroup);
+  // chair.body.collides(studentCollisionGroup, playerCollisionGroup);
+  // -----------------------------------------------------------
+
   let table1 = game.add.sprite (650, 170, 'sprites', 'table.png');
+
+
   let table2 = game.add.sprite (600, 170, 'sprites', 'table.png');
   let table3 = game.add.sprite (550, 170, 'sprites', 'table.png');
   let table4 = game.add.sprite (500, 170, 'sprites', 'table.png');
@@ -80,107 +165,120 @@ function create() {
   let table25 = game.add.sprite (200, 515, 'sprites', 'table.png');
   let table26 = game.add.sprite (600, 50, 'sprites', 'table.png');
   let table27 = game.add.sprite (550, 50, 'sprites', 'table.png');
+  let table28 = game.add.sprite (700, 170, 'sprites', 'table.png');
   //door
   let door = game.add.sprite (20, 500, 'sprites', 'door.png');
 
-  katie.scale.setTo(0.4,0.4);
-  table1.scale.setTo(0.5,0.5);
-  table2.scale.setTo(0.5,0.5);
-  table3.scale.setTo(0.5,0.5);
-  table4.scale.setTo(0.5,0.5);
-  table5.scale.setTo(0.5,0.5);
-  table6.scale.setTo(0.5,0.5);
-  table7.scale.setTo(0.5,0.5);
-  table8.scale.setTo(0.5,0.5);
-  table9.scale.setTo(0.5,0.5);
-  table10.scale.setTo(0.5,0.5);
-  table11.scale.setTo(0.5,0.5);
-  table12.scale.setTo(0.5,0.5);
-  table13.scale.setTo(0.5,0.5);
-  table14.scale.setTo(0.5,0.5);
-  table15.scale.setTo(0.5,0.5);
-  table16.scale.setTo(0.5,0.5);
-  table17.scale.setTo(0.5,0.5);
-  table18.scale.setTo(0.5,0.5);
-  table19.scale.setTo(0.5,0.5);
-  table20.scale.setTo(0.5,0.5);
-  table21.scale.setTo(0.5,0.5);
-  table22.scale.setTo(0.5,0.5);
-  table23.scale.setTo(0.5,0.5);
-  table24.scale.setTo(0.5,0.5);
-  table25.scale.setTo(0.5,0.5);
-  table26.scale.setTo(0.5,0.5);
-  table27.scale.setTo(0.5,0.5);
-    //door
-  door.scale.setTo(0.5,0.5);
+  //resize
+  // katie.scale.setTo(0.4,0.4);
+  // table1.scale.setTo(0.5,0.5);
+  // table2.scale.setTo(0.5,0.5);
+  // table3.scale.setTo(0.5,0.5);
+  // table4.scale.setTo(0.5,0.5);
+  // table5.scale.setTo(0.5,0.5);
+  // table6.scale.setTo(0.5,0.5);
+  // table7.scale.setTo(0.5,0.5);
+  // table8.scale.setTo(0.5,0.5);
+  // table9.scale.setTo(0.5,0.5);
+  // table10.scale.setTo(0.5,0.5);
+  // table11.scale.setTo(0.5,0.5);
+  // table12.scale.setTo(0.5,0.5);
+  // table13.scale.setTo(0.5,0.5);
+  // table14.scale.setTo(0.5,0.5);
+  // table15.scale.setTo(0.5,0.5);
+  // table16.scale.setTo(0.5,0.5);
+  // table17.scale.setTo(0.5,0.5);
+  // table18.scale.setTo(0.5,0.5);
+  // table19.scale.setTo(0.5,0.5);
+  // table20.scale.setTo(0.5,0.5);
+  // table21.scale.setTo(0.5,0.5);
+  // table22.scale.setTo(0.5,0.5);
+  // table23.scale.setTo(0.5,0.5);
+  // table24.scale.setTo(0.5,0.5);
+  // table25.scale.setTo(0.5,0.5);
+  // table26.scale.setTo(0.5,0.5);
+  // table27.scale.setTo(0.5,0.5);
+  // table28.scale.setTo(0.5,0.5);
+  // door.scale.setTo(0.5,0.5);
+  // violeta.scale.setTo(0.4,0.4);
   // students.scale.setTo(0.4,0.4);
-
-  game.physics.p2.enable([ table1, table2, table3, table4, table5, table6,
-                           table7, table8, table9, table10, table11, table12,
-                           door, table13, table14, table15, table16, table17,
-                           table18, table19, table20, table21, table22, table23,
-                           table24, table25, table26, table27 ]);
-
-  //make static
-  table1.body.static = true;
-  table2.body.static = true;
-  table3.body.static = true;
-  table4.body.static = true;
-  table5.body.static = true;
-  table6.body.static = true;
-  table7.body.static = true;
-  table8.body.static = true;
-  table9.body.static = true;
-  table10.body.static = true;
-  table11.body.static = true;
-  table12.body.static = true;
-  table13.body.static = true;
-  table14.body.static = true;
-  table15.body.static = true;
-  table16.body.static = true;
-  table17.body.static = true;
-  table18.body.static = true;
-  table19.body.static = true;
-  table20.body.static = true;
-  table21.body.static = true;
-  table22.body.static = true;
-  table23.body.static = true;
-  table24.body.static = true;
-  table25.body.static = true;
-  table26.body.static = true;
-  table27.body.static = true;
-  //door
-  door.body.static = true;
-
-  // table9.body.setZeroDamping();
-	// table9.body.fixedRotation = true;
+  //
+  //
+  // game.physics.p2.enable([ table1, table2, table3, table4, table5, table6,
+  //                          table7, table8, table9, table10, table11, table12,
+  //                          door, table13, table14, table15, table16, table17,
+  //                          table18, table19, table20, table21, table22, table23,
+  //                          table24, table25, table26, table27, table28
+  //                       ]);
+  //
+    //  table1.body.collides(playerCollisionGroup, studentCollisionGroup);
+  // //make static
+  // table1.body.static = false;
+  // table2.body.static = true;
+  // table3.body.static = true;
+  // table4.body.static = true;
+  // table5.body.static = true;
+  // table6.body.static = true;
+  // table7.body.static = true;
+  // table8.body.static = true;
+  // table9.body.static = true;
+  // table10.body.static = true;
+  // table11.body.static = true;
+  // table12.body.static = true;
+  // table13.body.static = true;
+  // table14.body.static = true;
+  // table15.body.static = true;
+  // table16.body.static = true;
+  // table17.body.static = true;
+  // table18.body.static = true;
+  // table19.body.static = true;
+  // table20.body.static = true;
+  // table21.body.static = true;
+  // table22.body.static = true;
+  // table23.body.static = true;
+  // table24.body.static = true;
+  // table25.body.static = true;
+  // table26.body.static = true;
+  // table27.body.static = true;
+  // table28.body.static = true;
+  // //door
+  // door.body.static = true;
 
   cursors = game.input.keyboard.createCursorKeys();
 
 }
 
+// -----------------------------------------------------------
+function hitStudent(body1, body2) {
+  body2.sprite.alpha -= 0.5;
+}
+
+// Trying tables collision
+// function hitChair(body1, body2) {
+//   body2.sprite.alpha -= 1;
+// }
+
+// -----------------------------------------------------------
 
 function update() {
-
-  // let cursors = game.input.keyboard.createCursorKeys();
 
   katie.body.setZeroVelocity();
 
   if (cursors.left.isDown)
     {
-      katie.body.moveLeft(200);
+      katie.body.moveLeft(350);
     }
   else if (cursors.right.isDown)
     {
-      katie.body.moveRight(200);
+      katie.body.moveRight(350);
     }
   if (cursors.up.isDown)
     {
-      katie.body.moveUp(200);
+      katie.body.moveUp(350);
     }
   else if (cursors.down.isDown)
     {
-      katie.body.moveDown(200);
+      katie.body.moveDown(350);
     }
   // if (!game.camera.atLimit.x)
   // {
@@ -190,4 +288,8 @@ function update() {
   // {
   //   ground.tilePosition.y += (katie.body.velocity.y * 16) * game.time.physicsElapsed;
   // }
+}
+
+function render() {
+  game.debug.text('Move the students out of the way!', 32, 32);
 }
